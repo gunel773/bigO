ae84c6123b13e0720c7de58781d721490e30af26
gunel773 <gunelmg@code.edu.az>
2023-10-19 01:13:18 +0400

    new task file

---
+++ DOORSHELF.cs
@@ -0,0 +1,14 @@
+﻿int doorLen = 90;
+int doorHeight = 210;
+int shHeight = 80;
+int shLen = 60;
+int shwide = 70;
+if (shHeight < doorHeight && shLen < doorLen && shHeight < doorLen && shLen < doorHeight &&
+    shwide < doorLen && shwide < doorHeight)
+{
+    Console.WriteLine("skaf qapidan her cur kecir");
+}
+else
+{
+    Console.WriteLine("qapini deyisin");
+}
