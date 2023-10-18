6e5a8e114c41340441c60345b1481cdf0c422290
gunel773 <gunelmg@code.edu.az>
2023-10-19 00:33:46 +0400

    new task file adding

---
+++ csbigo.cs
@@ -0,0 +1,23 @@
+﻿//iki ededin ceminin yoxlanmasi
+Console.WriteLine("ededleri daxil edin:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int count = 0;
+int result = a + b;
+while (result > 0)
+{
+    result /= 10;
+    count++;
+}
+if (count == 2)
+{
+    Console.WriteLine("ededlerin cemi ikireqemlidir");
+} 
+else if(count == 3)
+{
+    Console.WriteLine("ededlerin cemi ucreqemlidir");
+}
+else
+{
+    Console.WriteLine("cem nezere alinmir");
+}
